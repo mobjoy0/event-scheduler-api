e86929f32eea062ebba1ea2eba7adaf33cae5f09
mobjoy0 <wessimbenna@hotmail.com>
2025-06-16 12:09:06 +0100

    initial commit

---
+++ src/main/java/com/wessim/event_scheduler_api/EventSchedulerApiApplication.java
@@ -0,0 +1,13 @@
+package com.wessim.event_scheduler_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSchedulerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSchedulerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wessim/event_scheduler_api/EventSchedulerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wessim.event_scheduler_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSchedulerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
